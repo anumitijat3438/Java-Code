880c8fad736131f558a08ff422a5d126369de85a
AnumitiJat <anumitijat3438@gmail.com>
2025-01-31 04:51:00 -0800

    Add files via upload

---
+++ Reverse_a_Number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Reverse_a_Number{
+    public static void main(String[] args){
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any Number:");
+        num=sc.nextInt();
+        int rem,rev=0;
+        while(num>0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println("Reverse: " + rev);
+        }
+}
