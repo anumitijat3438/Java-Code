df77eb4f2981f00e67e9e93c7246ae074d7c4585
AnumitiJat <anumitijat3438@gmail.com>
2025-04-12 09:27:02 +0530

    Add files via upload

---
+++ LinkedList/Palindromelinkedlist.java
@@ -0,0 +1,93 @@
+public class Palindromelinkedlist {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    Node getMiddle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    boolean isPalindrome() {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Node midNode = getMiddle(head);
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
+    }
+
+    public void print() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linkedlist is empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Palindromelinkedlist ll = new Palindromelinkedlist();
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        System.out.println(ll.isPalindrome());
+         
+    }
+
+}
